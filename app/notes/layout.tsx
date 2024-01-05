632718c15369c198d440e2cd11ebfef7ee332e75
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-01-05 14:18:01 +0530

    Created Notes Layout

---
+++ app/notes/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from './_components/navbar';
+
+const NotesLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+      <main className='m-auto max-w-7xl p-4'>
+        {children}
+      </main>
+    </>
+  )
+}
+
+export default NotesLayout
