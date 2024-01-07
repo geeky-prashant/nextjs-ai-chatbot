c6883dfe907e2afd4a61b0461fb464e1edeec8ed
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-01-07 13:42:51 +0530

    Wrote API for Creating Notes

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,5 +1,35 @@
+import { createNoteSchema } from "@/lib/validation/note";
+import { auth } from "@clerk/nextjs";
+import prisma from "./../../../lib/db/prisma";
+
 export async function POST(req: Request) {
   try {
+    const body = await req.json();
+
+    const parseResult = createNoteSchema.safeParse(body);
+
+    if (!parseResult.success) {
+      console.log(parseResult.error);
+      return Response.json({ error: "Invalid Input" }, { status: 400 });
+    }
+
+    const { title, content } = parseResult.data;
+
+    const { userId } = auth();
+
+    if (!userId) {
+      return Response.json({ error: "Unauthorised" }, { status: 401 });
+    }
+
+    const note = prisma.note.create({
+      data: {
+        title,
+        content,
+        userId,
+      },
+    });
+
+    return Response.json({ note }, { status: 401 });
   } catch (error) {
     console.log(error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
