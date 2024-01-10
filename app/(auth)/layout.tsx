524fec5a2f672c25886d0bb095a0009790fd9467
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-01-10 14:00:34 +0530

    Customised Login/SignUp page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ const AuthLayout = ({
   children: React.ReactNode
 }) => {
   return (
-    <div className="flex items-center justify-center h-full">
+    <div className="flex items-center justify-center h-full bg-gradient-to-r from-rose-100 to-teal-100]">
       {children}
     </div>
   )
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -15,16 +15,16 @@ const HomePage = () => {
 
   return (
     <main
-      className="flex items-center justify-center flex-col h-screen gap-5 bg-gradient-to-r from-rose-100 to-teal-100"
+      className="flex items-center justify-center flex-col h-full gap-5 bg-gradient-to-r from-rose-100 to-teal-100"
     >
       <div className="flex items-center gap-4">
-        <Image src={Logo} alt="Logo" width={90} height={90} />
+        <Image src={Logo} alt="Logo" width={80} height={80} />
         <span className="font-extrabold tracking-tight text-3xl lg:text-5xl text-[#1C274C]">365Bot</span>
       </div>
-      <p className="text-center max-w-md px-4 text-xl md:text-base">An intelligent note-taking app with AI integration, built with cutting-edge technologies</p>
+      <p className="text-center max-w-md px-4 text-md md:text-base">An intelligent note-taking app with AI integration, built with cutting-edge technologies</p>
       <Button
         size="lg"
-        className="bg-gradient-to-r from-[#0F9E7B] to-[#1a6c57] mt-8"
+        className="bg-gradient-to-r from-[#0F9E7B] to-[#1a6c57] mt-4"
       >
         <ArrowRight size={20} className="mr-1" />
         <Link href="/notes" className="font-bold text-base">
